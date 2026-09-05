c5cad777491c3c9c5f75114818327572af03ce43
Арман Берик <berikarman7@gmail.com>
2026-09-06 02:32:08 +0500

    Create words.js

---
+++ words.js
@@ -0,0 +1,28 @@
+const dictionary = [
+
+    {
+        french: "bonjour",
+        russian: "здравствуйте",
+        category: "basic"
+    },
+
+    {
+        french: "merci",
+        russian: "спасибо",
+        category: "basic"
+    },
+
+    {
+        french: "maison",
+        russian: "дом",
+        category: "home"
+    },
+
+    {
+        french: "manger",
+        russian: "есть",
+        category: "verbs"
+    }
+
+    // ... дальше остальные слова
+];
